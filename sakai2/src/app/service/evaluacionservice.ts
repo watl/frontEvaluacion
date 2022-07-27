f4f135c13874b9729555e4735a138f153b66d99e
watl <walter.martinez04@gmail.com>
2022-07-26 21:12:34 -0600

    creacion de proyecto front angular

---
+++ sakai2/src/app/service/evaluacionservice.ts
@@ -0,0 +1,33 @@
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class EvaluacionService {
+
+constructor(private http: HttpClient) { }
+    httpOptions = {
+    headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+    })
+    }
+    getData(){
+
+    return this.http.get('/api/Employee');  //https://localhost:44352/ webapi host url
+    }
+
+    postData(formData){
+    return this.http.post('/api/Employee',formData);
+    }
+
+    putData(id,formData){
+    return this.http.put('/api/Employee/'+id,formData);
+    }
+    deleteData(id){
+    return this.http.delete('/api/Employee/'+id);
+    }
+
+}
